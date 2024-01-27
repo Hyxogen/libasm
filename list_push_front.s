355d91eca3ffec0aa8c967bad0a65ab68ce9ad3e
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2024-01-27 18:28:50 +0100

    fixup! add ft_list_push_front

---
+++ list_push_front.s
@@ -0,0 +1,38 @@
+BITS 64
+global ft_list_push_front
+extern malloc
+
+; void ft_list_push_front(t_list **begin_list, void *data)
+; {
+;   t_list *link = malloc(sizeof t_list);
+;   if (!link)
+;     return;
+;
+;   link->data = data;
+;   link->next = *begin_list;
+;
+;   *begin_list = link;
+; }
+
+ft_list_push_front:
+  push rbp
+  mov rbp, rsp
+
+  push rbx
+  push r12
+  mov rbx, rdi
+  mov r12, rsi
+
+  mov rdi, 0x10
+  call malloc wrt ..plt
+  jz .end
+
+  mov [rax], r12
+  mov rsi, [rbx]
+  mov [rax + 0x8], rsi
+  mov [rbx], rax
+.end:
+  pop r12
+  pop rbx
+  pop rbp
+  ret
